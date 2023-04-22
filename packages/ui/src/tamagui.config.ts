de0777b0290c192c606c11f3e62ac15d7ba3dd0a
wonyoung.choi <mononoke.choi@gmail.com>
2023-04-22 12:07:33 +0900

    simplify tamgaui configuration

--- packages/ui/src/tamagui.config.ts
+++ packages/ui/src/tamagui.config.ts
@@ -1,61 +1,18 @@
-import { createAnimations } from '@tamagui/animations-react-native';
-import { createInterFont } from '@tamagui/font-inter';
-import { createMedia } from '@tamagui/react-native-media-driver';
-import { shorthands } from '@tamagui/shorthands';
-import { themes, tokens } from '@tamagui/themes';
+import { config } from '@tamagui/config';
 import { createTamagui } from 'tamagui';
 
-const animations = createAnimations({
-  bouncy: {
-    type: 'spring',
-    damping: 10,
-    mass: 0.9,
-    stiffness: 100,
-  },
-  lazy: {
-    type: 'spring',
-    damping: 20,
-    stiffness: 60,
-  },
-  quick: {
-    type: 'spring',
-    damping: 20,
-    mass: 1.2,
-    stiffness: 250,
-  },
+Object.assign(config.media, {
+  tiny: { maxWidth: 500 },
+  gtTiny: { minWidth: 500 + 1 },
+  small: { maxWidth: 620 },
+  gtSmall: { minWidth: 620 + 1 },
+  medium: { maxWidth: 780 },
+  gtMedium: { minWidth: 780 + 1 },
+  large: { maxWidth: 900 },
+  gtLarge: { minWidth: 900 + 1 },
 });
 
-const interFont = createInterFont();
-
-const tamaguiConfig = createTamagui({
-  animations,
-  defaultTheme: 'dark',
-  shouldAddPrefersColorThemes: false,
-  themeClassNameOnRoot: false,
-  shorthands,
-  fonts: {
-    heading: interFont,
-    body: interFont,
-  },
-  themes,
-  tokens,
-  media: createMedia({
-    xs: { maxWidth: 660 },
-    sm: { maxWidth: 800 },
-    md: { maxWidth: 1020 },
-    lg: { maxWidth: 1280 },
-    xl: { maxWidth: 1420 },
-    xxl: { maxWidth: 1600 },
-    gtXs: { minWidth: 660 + 1 },
-    gtSm: { minWidth: 800 + 1 },
-    gtMd: { minWidth: 1020 + 1 },
-    gtLg: { minWidth: 1280 + 1 },
-    short: { maxHeight: 820 },
-    tall: { minHeight: 820 },
-    hoverNone: { hover: 'none' },
-    pointerCoarse: { pointer: 'coarse' },
-  }),
-});
+const tamaguiConfig = createTamagui(config);
 
 export type TamaguiConfig = typeof tamaguiConfig;
 export default tamaguiConfig;
